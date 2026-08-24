f4b9fa91d3024ea52d2d184df67977e49ba7060d
Tan Nugraha <cvdv0509@gmail.com>
2026-08-24 21:04:35 +0700

    fix: request Threads keyword search OAuth permission

--- app/api/oauth/threads/start/route.ts
+++ app/api/oauth/threads/start/route.ts
@@ -23,7 +23,7 @@ export async function GET(request: Request) {
   const url = new URL('https://threads.net/oauth/authorize');
   url.searchParams.set('client_id', THREADS_APP_ID);
   url.searchParams.set('redirect_uri', REDIRECT_URI);
-  url.searchParams.set('scope', 'threads_basic,threads_content_publish,threads_manage_replies,threads_read_replies,threads_profile_discovery');
+  url.searchParams.set('scope', 'threads_basic,threads_content_publish,threads_manage_replies,threads_read_replies,threads_profile_discovery,threads_keyword_search');
   url.searchParams.set('response_type', 'code');
   url.searchParams.set('state', state);
 
